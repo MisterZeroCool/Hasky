3afeb2bfeabb10342689e0cb628f3f3ad0839d4a
SlyRedFox <one1970zero@mail.ru>
2023-09-08 17:57:14 +0300

    Login

--- app/src/main/java/ru/agavrilyuk/MainActivity.kt
+++ app/src/main/java/ru/agavrilyuk/MainActivity.kt
@@ -2,10 +2,38 @@ package ru.agavrilyuk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import ru.agavrilyuk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //initialize animations
+
+        var fade_in = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        var bottom_down = AnimationUtils.loadAnimation(this, R.anim.button_down)
+
+        //setting the bottom down animations on top layout
+
+        binding.topLinearLayout.animation = bottom_down
+
+        //create handler for layouts
+        var handler = Handler()
+        var runnable = Runnable {
+            binding.cardView.animation = fade_in
+            binding.cardView2.animation = fade_in
+            binding.cardView3.animation = fade_in
+            binding.textView.animation = fade_in
+            binding.textView2.animation = fade_in
+            binding.registerLayout.animation = fade_in
+        }
+
+        handler.postDelayed(runnable,1000)
     }
 }
